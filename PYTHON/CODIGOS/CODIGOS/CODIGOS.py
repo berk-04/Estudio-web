4e0b47dfd6a1ce84e9ad8026ecce93ea0a85d6f1
juan garcía <juan.garcia14@utp.edu.co>
2022-06-11 20:32:35 -0500

    Update 11/06/2022

--- PYTHON/CODIGOS/CODIGOS/CODIGOS.py
+++ PYTHON/CODIGOS/CODIGOS/CODIGOS.py
@@ -74,6 +74,62 @@
 # print(m.format(n))
 
 
-a="apa hola\raasas"
+# a="apa hola\raasas"
 
-print(a)
+# print(a)
+
+# string=input("Ingrese una cadena de texto: ")
+# string_1=input("Ingrese una cadena de texto: ")
+# longitud_cadena=len(string)
+
+# def first_middle_end(string,longitud_cadena):
+#     mitad=longitud_cadena//2
+#     a=string[0]+string[mitad]+string[longitud_cadena-1]
+#     return a
+
+# def three_middle(string,longitud_cadena):
+#     mitad=longitud_cadena//2
+#     print(string[mitad-1:mitad+2])
+
+# def concatenate(string,string_1):
+#     longitud_cadena=len(string)
+#     longitud_cadena_1=len(string_1)
+#     a=first_middle_end(string,longitud_cadena)
+#     b=first_middle_end(string_1,longitud_cadena_1)
+#     string=a[0]+b[0]+a[1]+b[1]+a[2]+b[2]
+#     return string
+
+
+# print(first_middle_end(string,longitud_cadena))
+# three_middle(string,longitud_cadena)
+# print(concatenate(string,string_1))
+
+
+# def saludar():
+#     print("hola")
+
+# saludar()
+
+
+a="hola"
+b="paulina benjumea mejia"
+print(len(a))
+
+print(a[0])
+
+print(a[4:])
+
+print(a.upper())    
+print(a.lower())
+print(a.replace("la","LA"))
+print(a.capitalize())
+print(b.title())
+print(b.encode())
+m=b.split(" ")
+
+a="texto"
+b="Esto es un {0} de ayuda {2} {1} {4} {3}"
+
+print(b.format(a,3,7,8,"hola"))
+
+print(len(m))
